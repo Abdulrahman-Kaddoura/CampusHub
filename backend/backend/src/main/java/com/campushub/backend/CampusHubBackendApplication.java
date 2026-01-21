7347e760919c0598ad6d5240bef27365b12f575d
abdkad123 <akaddoura123@gmail.com>
2026-01-21 20:38:52 +0200

    initial commit

---
+++ backend/backend/src/main/java/com/campushub/backend/CampusHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.campushub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusHubBackendApplication.class, args);
+	}
+
+}
