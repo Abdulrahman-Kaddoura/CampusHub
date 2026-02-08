a04dc3546611222913c1b1c5f544efc20517d268
antarhousni-source <antarhousni@gmail.com>
2026-02-08 23:56:08 +0200

    Update MarketPlace.jsx

--- campus-hub/src/pages/MarketPlace/MarketPlace.jsx
+++ campus-hub/src/pages/MarketPlace/MarketPlace.jsx
@@ -1,11 +1,25 @@
-import { Section } from "../../components/ProductSection"; 
+import { Section } from "../../components/ProductSection";
+import "./MarketPlace.css";
 
 function MarketPlace() {
-    return (
-        <>
-            <Section/>
-        </>
-    );
+  return (
+    <div className="marketplace">
+      {/* Hero Banner */}
+      <div className="marketplace-hero">
+        <h1>Market Place</h1>
+        <p>Buy from and sell to other students</p>
+      </div>
+
+      {/* Search Bar */}
+      <div className="marketplace-search">
+        <input type="text" placeholder="Search for items..." />
+        <button>Search</button>
+      </div>
+
+      {/* Existing Marketplace Section */}
+      <Section />
+    </div>
+  );
 }
 
 export default MarketPlace;
