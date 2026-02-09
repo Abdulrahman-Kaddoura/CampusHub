4ea7c592e2eb7c09acd985823689ef318134910a
antarhousni-source <antarhousni@gmail.com>
2026-02-09 13:30:31 +0200

    Update ProductSection.jsx

--- campus-hub/src/components/ProductSection.jsx
+++ campus-hub/src/components/ProductSection.jsx
@@ -1,30 +1,24 @@
-import { products } from "../products";
 import { ProductCard } from "./ProductCard";
 import "./ProductSection.css";
 import arrow from "../assets/arrow.svg";
 
-export const Section = ({ title = "Books and Stationery" }) => {
+export const Section = ({ title = "Books and Stationery", products = [] }) => {
   return (
     <div className="product-section">
-      <div className="sectionTitle">
-        <div className="title-name">
-          <h3>
-            Shop for best deals on <span>{title}</span>
-          </h3>
-          <div className="view">
-            <p>view all</p>
-            <img src={arrow} alt="arrow" />
-          </div>
+      <div className="section-header">
+        <h3>
+          Shop for best deals on <span>{title}</span>
+        </h3>
+        <div className="view-all">
+          <p>View All</p>
+          <img src={arrow} alt="arrow" />
         </div>
-        <hr />
       </div>
 
-      <div className="cards-container">
-        <div className="cards">
-          {products.map((product) => (
-            <ProductCard key={product.id} data={product} />
-          ))}
-        </div>
+      <div className="cards">
+        {products.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
       </div>
     </div>
   );
