617137a30eec9552a28fd8e44a16635a9e2a4398
antarhousni-source <antarhousni@gmail.com>
2026-02-09 14:07:58 +0200

    Update products.js

--- campus-hub/src/products.js
+++ campus-hub/src/products.js
@@ -1,48 +1,43 @@
-import product1 from "./assets/book.jpg";
-import product2 from "./assets/book2.jpg";
-import product3 from "./assets/book3.webp";
-import product4 from "./assets/book4.webp";
-  
-  export const products = [
-    {
-        id : 1,
-        productName : "Thomas Calculus 11th Edition",
-        author : "Sarah Williams",
-        price : 55.0,
-        description: "Brand New. No writings in it",
-        productImage : product1
-    }, 
+import book1 from "./assets/book.jpg";
+import book2 from "./assets/book2.jpg";
+import book3 from "./assets/book3.webp";
+import book4 from "./assets/book4.webp";
 
-    {
-        id : 2,
-        productName : "Probability and Random Process for Electrical and Computer Engineers",
-        author : "James Hugh",
-        price : 49.0,
-        description: "Fairly used, still in good condition. No writings in it",
-        productImage : product2
-    }, 
-    
-    {
-        id : 3,
-        productName : "Introduction to the theory of Computation",
-        author : "John Doe",
-        price : 22.0,
-        description: "Brand New. No writings in it",
-        productImage : product3
-    }, 
-    
-    {
-        id : 4,
-        productName : "Operating System",
-        author : "Sarah Williams",
-        price : 55.0,
-        description: "Brand New. No writings in it",
-        productImage : product4
-    }
-]
-
-export const SectionTitle = [
-       {title1 : "Books and Stationery"},
-       {title2 : "Room Decor"},
-       {title3 : "Clothing"},
-]
+export const products = [
+  {
+    id: 1,
+    productName: "Thomas Calculus 11th Edition",
+    author: "Sarah Williams",
+    price: 55,
+    description: "Brand New. No writings in it",
+    productImage: book1,
+    category: "Books",
+  },
+  {
+    id: 2,
+    productName: "Probability and Random Process",
+    author: "James Hugh",
+    price: 49,
+    description: "Fairly used, still in good condition",
+    productImage: book2,
+    category: "Books",
+  },
+  {
+    id: 3,
+    productName: "Introduction to the theory of Computation",
+    author: "John Doe",
+    price: 22,
+    description: "Brand New. No writings in it",
+    productImage: book3,
+    category: "Clothing",
+  },
+  {
+    id: 4,
+    productName: "Operating System",
+    author: "Sarah Williams",
+    price: 55,
+    description: "Brand New. No writings in it",
+    productImage: book4,
+    category: "Room Decor",
+  },
+];
