5a2e3571d3fe201582457579d4c6b7e21bf14440
abdkad123 <akaddoura123@gmail.com>
2026-02-22 22:22:55 +0200

    started with dorm backend

--- backend/backend/src/main/java/com/campushub/backend/controllers/dorm/DormController.java
+++ backend/backend/src/main/java/com/campushub/backend/controllers/dorm/DormController.java
@@ -1,4 +1,11 @@
 package com.campushub.backend.controllers.dorm;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dorm")
+@Tag(name = "Dorm", description = "Dorm related operations")
 public class DormController {
 }
